04a2b8e884efa06f9531daf55eb707e44c006531
Connor Ashley <connorashley@Connors-MacBook-Pro.local>
2025-12-26 00:06:37 +0800

    Fix: Reverting CSP config to defaults

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; script-src * 'unsafe-inline' 'unsafe-eval' data: blob:; style-src * 'unsafe-inline'; img-src * data: blob:; font-src * data: blob:; connect-src *;",
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
